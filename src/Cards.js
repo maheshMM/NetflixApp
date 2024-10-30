98aab5a4fb9d26478bedbfa66af48e250ca34116
unknown <maheshmedavarapu@gmail.com>
2024-10-30 01:14:48 +0000

    feed/stylefixs: stylefix

--- src/Cards.js
+++ src/Cards.js
@@ -3,14 +3,14 @@ import './App.css';
 const Cards = ({ data, watchnowHandle, addtoWatchListHandle}) => {
     return (
 
-        <div className="card" style = {{width :'18rem', margin:"10px" , border:"10px", borderColor:"black"}}>
+        <div className="card" style = {{width :'18rem', margin:"10px" , border:"20px", borderColor:"yellow"}}>
             <img src={data.Poster} style = {{width :'250px', height:"250px"}} className="card-img-top" alt={data.Title} />
             <div className="card-body">
                 <h5 className="card-title">{data.Title}</h5>
                 <p className="card-text">{data.Title}</p>
-                <button className = "watchNow" onClick={()=>watchnowHandle(data.Title)} >Watch Now</button>
+                <button className = "watchNow" onClick={()=>watchnowHandles(data.Title)} >Watch Now</button>
                 <button className="watchLater" onClick = {()=>addtoWatchListHandle(data.Title)}>Add to WatchList</button>
-        
+                
             </div>
         </div>
 
